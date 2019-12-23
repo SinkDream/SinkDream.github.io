793f709bf1c05f79513ff219ddcc1eb138a75218
xiangqiling <xiangqiling@haier.com>
2019-12-23 14:03:28 +0800

    [Feature]12月23日的新文案

---
+++ precache-manifest.6c96599cd21862ab0c906d7460ef00d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a980f83109fa71fcb83159938d934c5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a390f3c1c09594cf845f",
+    "url": "/static/css/2.bea6f0b1.chunk.css"
+  },
+  {
+    "revision": "f68437038659c3c9a409",
+    "url": "/static/css/main.ce20bd61.chunk.css"
+  },
+  {
+    "revision": "a390f3c1c09594cf845f",
+    "url": "/static/js/2.6b89c9f6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6b89c9f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f68437038659c3c9a409",
+    "url": "/static/js/main.131daf00.chunk.js"
+  },
+  {
+    "revision": "6b552844428d7e7be7f4",
+    "url": "/static/js/runtime-main.e4a5db44.js"
+  }
+]);
